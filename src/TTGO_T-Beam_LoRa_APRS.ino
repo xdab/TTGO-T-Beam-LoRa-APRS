79dfc0cc8f2d53c7a9e52463292d0d297d5a6668
Rysiek Labus (SQ9MDD) <rysieklabus@gmail.com>
2021-09-13 23:24:03 +0200

    TTGO T-Beam 0.7 voltage scaling problem #65
    fixed thanks to Luca IU2FRL

--- src/TTGO_T-Beam_LoRa_APRS.ino
+++ src/TTGO_T-Beam_LoRa_APRS.ino
@@ -356,7 +356,7 @@ void batt_read(){
   BattVolts = axp.getBattVoltage()/1000;
   InpVolts = axp.getVbusVoltage()/1000;
 #else
-  BattVolts = analogRead(35)*7.221/4096;
+  BattVolts = analogRead(35)*7.221/8192;    // fixed thanks to Luca IU2FRL 
 #endif
 }
 
