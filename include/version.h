a01aa4f8fd022c94d57c8e2f51fe34ffc900598f
Przemyslaw Wcislo <przemyslawxdab@gmail.com>
2024-10-26 10:47:30 +0200

    remove unused files

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "9"
+  #define BUILD_NUMBER "1"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.4.9-5b255e1 - 2024-10-26 10:37:29.418478"
+  #define VERSION "v0.4.1-3798dc5 - 2024-10-26 10:45:11.162665"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.4.9-5b255e1"
+  #define VERSION_SHORT "v0.4.1-3798dc5"
 #endif
